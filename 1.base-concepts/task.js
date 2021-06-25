4a5aeb68ae89f54f3758cb2b5992bc04679b6a4c
Andrey.Sosnin <woodogul@yandex.ru>
2021-06-25 19:19:12 +0300

    Задача 1 и 2
    * Реализация функции для решения квадратных уравнений,
    * Калькулятор для расчёта выплат по ипотеке

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,41 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant === 0) {
+    return [-b / (2 * a)]
+  } else if (discriminant > 0) {
+    return [
+      (-b + Math.sqrt(discriminant)) / (2 * a),
+      (-b - Math.sqrt(discriminant)) / (2 * a)
+    ]
+  } else if (discriminant < 0) return [];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  const percentClean = Number(percent);
+  const contributionClean = Number(contribution);
+  const amountClean = Number(amount);
+  const dateNow = new Date()
+  if (isNaN(percentClean)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  }
 
-  // код для задачи №2 писать здесь
+  if (isNaN(contributionClean)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  }
 
-  return totalAmount;
+  if (isNaN(amountClean)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  }
+
+  const loanBody = amountClean - contributionClean;
+
+  const loanTermInMonths = date.getMonth() - dateNow.getMonth() +
+    (12 * (date.getFullYear() - dateNow.getFullYear()));
+  const P = percentClean / 100 / 12;
+  const payment = loanBody * (P + P / ((Math.pow(1 + P, loanTermInMonths)) - 1));
+  totalAmount = payment * loanTermInMonths;
+  return Number(totalAmount.toFixed(2));
 }
