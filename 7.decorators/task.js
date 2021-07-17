07d777099816e1adec0b21a5b55d08817954aca5
Andrey.Sosnin <woodogul@yandex.ru>
2021-07-17 15:18:57 +0300

    Домашнее задание к лекции 7 «Декораторы»

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,55 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  return function (...args) {
+    const hash = args.join(':');
+    const idx = cache.findIndex((item) => item.hash === hash)
+    if (idx === -1) {
+      const result = func(...args);
+      cache.push({hash, result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      return `Вычисляем: ${result}`;
+    } else {
+      return `Из кэша: ${cache[idx].result}`
+    }
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let isFirst = true;
+  return function (...args) {
+    if (isFirst) {
+      isFirst = false;
+      func.apply(this, args);
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+        isFirst = true;
+      }, ms)
+    }
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout;
+  let isFirst = true;
+  let counter = 0;
+  return function (...args) {
+    if (isFirst) {
+      isFirst = false;
+      func.apply(this, args);
+      counter += 1;
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+        counter += 1;
+        isFirst = true;
+      }, ms)
+    }
+  }
 }
